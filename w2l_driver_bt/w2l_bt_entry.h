84b53136551427c72a8af6a14f72a1ca138195f1
zuorong hong <zuorong.hong@amlogic.com>
2025-08-12 14:37:49 +0800

    BT: amlbt remove Useless table of contents [1/1]
    PD#SWPL-227017
    
    Problem:
    AMLBT Git Content Splitting
    
    Solution:
    1.remove bt driver
    2.remove zigbee and thread
    4.add .gitignore file
    
    Verify:
    ok
    
    Change-Id: Icf6391096abf119df51d166525380fe45480e735
    Signed-off-by: zuorong hong <zuorong.hong@amlogic.com>

--- w2l_driver_bt/w2l_bt_entry.h
+++
@@ -1,8 +0,0 @@
-#ifndef __W2L_BT_ENTRY_H__
-#define __W2L_BT_ENTRY_H__
-
-#define AML_W2LS_VERSION    "W2L SDIO BT Driver release:2025-05-08-0946 +patch"
-#define AML_W2LU_VERSION    "W2L USB BT Driver release:2025-05-02-1453 +patch"
-
-#endif
-
