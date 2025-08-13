fe3af62043e394ee4514096000da7757f7ea4ce7
zuorong hong <zuorong.hong@amlogic.com>
2025-08-13 16:00:59 +0800

    BT: sync zigbee bin from trunk [1/1]
    PD#SWPL-227017
    
    Problem:
    AMLBT Git Content Splitting
    
    Solution:
    1.sync zigbee bin from trunk.
    2.from platform/hardware/amlogic/bluetooth git,branch plt-amlbt-release-16f76a7
    
    Verify:
    ok
    
    Change-Id: I4d9a0ae209bf667036eaa627061d6d75507e39a4
    Signed-off-by: zuorong hong <zuorong.hong@amlogic.com>

---
+++ zigbee/Android.mk
@@ -0,0 +1,290 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := dut_test_zc
+LOCAL_SRC_FILES := dut_test_zc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_chip_init
+LOCAL_SRC_FILES := z_chip_init
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_ed_join
+LOCAL_SRC_FILES := z_ed_join
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_nwk_formation
+LOCAL_SRC_FILES := z_nwk_formation
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_route_join
+LOCAL_SRC_FILES := z_route_join
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_set_role
+LOCAL_SRC_FILES := z_set_role
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_test_end
+LOCAL_SRC_FILES := z_test_end
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_throughput_rx
+LOCAL_SRC_FILES := z_throughput_rx
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_throughput_tx
+LOCAL_SRC_FILES := z_throughput_tx
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_stack_coor
+LOCAL_SRC_FILES := z_stack_coor
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_stack_ed
+LOCAL_SRC_FILES := z_stack_ed
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_stack_route
+LOCAL_SRC_FILES := z_stack_route
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_get_rssi
+LOCAL_SRC_FILES := z_get_rssi
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_get_channel
+LOCAL_SRC_FILES := z_get_channel
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_get_role
+LOCAL_SRC_FILES := z_get_role
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_help
+LOCAL_SRC_FILES := z_help
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_set_channel
+LOCAL_SRC_FILES := z_set_channel
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_get_devices
+LOCAL_SRC_FILES := z_get_devices
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_get_status
+LOCAL_SRC_FILES := z_get_status
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_leave
+LOCAL_SRC_FILES := z_leave
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_active_scan
+LOCAL_SRC_FILES := z_active_scan
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_permit_open
+LOCAL_SRC_FILES := z_permit_open
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_permit_close
+LOCAL_SRC_FILES := z_permit_close
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_throughput_rx_end
+LOCAL_SRC_FILES := z_throughput_rx_end
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_PROPRIETARY_MODULE := true
+#LOCAL_MODULE := threadbr_aml
+#LOCAL_SRC_FILES := threadbr_aml
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_MODULE_SUFFIX :=
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+#LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+#LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+#include $(BUILD_PREBUILT)
+#
+#include $(CLEAR_VARS)
+#LOCAL_PROPRIETARY_MODULE := true
+#LOCAL_MODULE := udpServer
+#LOCAL_SRC_FILES := udpServer
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_MODULE_SUFFIX :=
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+#LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+#LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+#include $(BUILD_PREBUILT)
+
+#endif # BOARD_HAVE_BLUETOOTH_AMLOGIC
