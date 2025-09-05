34a6e61eff66f2037b6b647708a89d03c68b1049
zuorong hong <zuorong.hong@amlogic.com>
2025-09-05 17:37:46 +0800

    BT: amlbt_release v3.3.7 for w2l release v3.0 [3/4]
    PD#SWPL-230852
    
    Problem:
    vlsi push W2l release v3.0
    
    Solution:
    1.update zigbee bin.
    
    Verify:
    ok
    
    Change-Id: I9bb9768d8a51eb0296f400ef1dff6bdad491e438
    Signed-off-by: zuorong hong <zuorong.hong@amlogic.com>

--- zigbee/Android.mk
+++ zigbee/Android.mk
@@ -265,6 +265,28 @@ LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
 LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_onoff_on
+LOCAL_SRC_FILES := z_onoff_on
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := z_onoff_off
+LOCAL_SRC_FILES := z_onoff_off
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+include $(BUILD_PREBUILT)
+
 #include $(CLEAR_VARS)
 #LOCAL_PROPRIETARY_MODULE := true
 #LOCAL_MODULE := threadbr_aml
